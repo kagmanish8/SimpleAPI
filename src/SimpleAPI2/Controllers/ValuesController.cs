fbb6e826dfb7ba934679c954d45b34c8fa309bbc
Manish Kag <manish.kag@beyondkey.com>
2020-04-07 18:41:47 +0530

    Change value

--- src/SimpleAPI2/Controllers/ValuesController.cs
+++ src/SimpleAPI2/Controllers/ValuesController.cs
@@ -14,7 +14,7 @@ namespace SimpleAPI2.Controllers
         [HttpGet]
         public ActionResult<IEnumerable<string>> Get()
         {
-            return new string[] { "value1", "value2" };
+            return new string[] { "value1a", "value2a" };
         }
 
         // GET api/values/5
